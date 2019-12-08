7932687e0f4a940744778575439e807a72b5807c
alex860111 <alex86011119@googlemail.com>
2019-12-08 12:28:40 +0100

    formatting

--- projects/ngx-formly-material-file/src/public-api.ts
+++ projects/ngx-formly-material-file/src/public-api.ts
@@ -3,20 +3,23 @@
  */
 
 export * from './lib/file-input/selected-file';
+
 export * from './lib/file-size/file-size.pipe';
+
+export * from './lib/file-type/file-type.component';
+
+export * from './lib/list-validators/max-files-error';
+export * from './lib/list-validators/min-files-error';
+export * from './lib/list-validators/total-filesize-error';
+export * from './lib/list-validators/file-list-validators';
 
 export * from './lib/validators/filename-forbidden-characters-error';
 export * from './lib/validators/filesize-error';
 export * from './lib/validators/max-filename-length-error';
 export * from './lib/validators/min-filename-length-error';
 export * from './lib/validators/file-extension-error';
-
-export * from './lib/list-validators/max-files-error';
-export * from './lib/list-validators/min-files-error';
-export * from './lib/list-validators/total-filesize-error';
-
-export * from './lib/file-type/file-type.component';
-export * from './lib/list-validators/file-list-validators';
 export * from './lib/validators/file-validators';
+
 export * from './lib/file-type-config';
+
 export * from './lib/file-type.module';
